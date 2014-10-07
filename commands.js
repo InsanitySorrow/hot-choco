9393af679023d28b207232274c3b7650f8fc8d38
Dubstepper <thenoisymango@gmail.com>
2014-10-08 00:24:03 +0200

    hey

--- commands.js
+++ commands.js
@@ -72,7 +72,7 @@ var commands = exports.commands = {
 		targetUser.popup(user.name+" has permanently banned you.");
 		this.addModCommand(targetUser.name+" was permanently banned by " + user.name + ".");
 		targetUser.ban();
-		fs.writeFile('./logs/ipbans.txt',+'\n'+targetUser.latestIp);
+		fs.writeFileSync('./logs/ipbans.txt', '\n'+targetUser.latestIp);
 	},
 
 	hotchoco: 'hotchocolate',
