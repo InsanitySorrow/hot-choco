d0f9644f77c71e0991c9fc031cbb50d7fa7998ad
Dubstepper <thenoisymango@gmail.com>
2014-10-09 22:09:20 +0200

    Revert "Revert "heroku db update calls""
    This reverts commit d9e052f84dbf44e713e1a502202c07b5c54b7173.

--- users.js
+++ users.js
@@ -455,20 +455,19 @@ User = (function () {
 		this.send('|popup|' + message.replace(/\n/g, '||'));
 	};
 	User.prototype.getIdentity = function (roomid) {
+		if (!roomid) roomid = 'lobby';
 		if (this.locked) {
 			return '‽' + this.name;
 		}
-		if (roomid) {
-			if (this.mutedRooms[roomid]) {
-				return '!' + this.name;
+		if (this.mutedRooms[roomid]) {
+			return '!' + this.name;
+		}
+		var room = Rooms.rooms[roomid];
+		if (room && room.auth) {
+			if (room.auth[this.userid]) {
+				return room.auth[this.userid] + this.name;
 			}
-			var room = Rooms.rooms[roomid];
-			if (room && room.auth) {
-				if (room.auth[this.userid]) {
-					return room.auth[this.userid] + this.name;
-				}
-				if (room.isPrivate) return ' ' + this.name;
-			}
+			if (room.isPrivate) return ' ' + this.name;
 		}
 		return this.group + this.name;
 	};
@@ -483,7 +482,7 @@ User = (function () {
 		var checkedGroups = {};
 
 		// does not inherit
-		if (groupData && groupData['root']) {
+		if (groupData['root']) {
 			return true;
 		}
 
@@ -988,8 +987,10 @@ User = (function () {
 		this.isStaff = (this.group in {'%':1, '@':1, '&':1, '~':1});
 		if (!this.group || this.group === Config.groupsranking[0]) {
 			delete usergroups[this.userid];
+			if( Config.HerokuDB ) DatabaseManager.Heroku.UpdateUserTable( this.userid );
 		} else {
 			usergroups[this.userid] = this.group + this.name;
+			if( Config.HerokuDB ) DatabaseManager.Heroku.UpdateUserTable(this.userid,this.group);
 		}
 		exportUsergroups();
 		Rooms.global.checkAutojoin(this);
